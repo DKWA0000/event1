a70b438df99839c7ad9848f5ec6f1b115f4e3eb0
dkwa0000 <joakim.tubring@gmail.com>
2025-12-04 16:41:58 +0100

    event application update

--- src/test/java/com/event/EventApplicationTests.java
+++ src/test/java/com/event/EventApplicationTests.java
@@ -1,13 +1,14 @@
 package com.event;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
-class EventApplicationTests {
+@ExtendWith(SpringExtension.class)
+    class EventApplicationTests {
 
-	/*@Test
-	void contextLoads() {
-	}*/
 
-}
+    @Test
+        void justRun() {}
+    }
